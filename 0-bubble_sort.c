15703f2067cce0d789e241628330931515454e04
colly-m <sindanicolly@gmail.com>
2023-11-19 12:15:26 -0800

    Bubbly sorty

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Function to sort integer array in ascending arder
+ * using bubble sort algorithm
+ * @array: pointer to array to sort through
+ * @size: poinet to the size of the array
+ * Return: Null
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
